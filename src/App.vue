0c02551d1998d87e51e02471e790367410714403
MrAmiD <nadymov2910@gmail.com>
2018-08-24 09:26:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <appCatalog>
+
+    </appCatalog>
+  </div>
+</template>
+
+<script>
+
+  import appCatalog from './components/catalog'
+
+  export default {
+    name: 'catalogPage',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      }
+    },
+    components: {
+      appCatalog,
+    }
+  }
+</script>
+
+<style lang="sass">
+/*bootstrap styles*/
+@import '../node_modules/bootstrap/scss/bootstrap'
+
+
+@import "assets/sass/vars"
+@import "assets/sass/fonts"
+
+*
+  outline: none!important
+  font-weight: 500
+html, body
+  height: auto
+body
+  font-size: 16px
+  min-width: 1280px
+  position: relative
+  line-height: 1.2
+  font-family: $default-font
+  opacity: 1
+  margin: 0px
+*
+  font-family: $default-font
+  outline: none!important
+h1
+  display: flex
+  align-items: center
+  font-family: 'ProximaNova', sans-serif
+</style>
---
+++ src/components/catalog.vue
@@ -0,0 +1,32 @@
+<!--Каталог компонент-->
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>
+          ComponentTemplate {{msg}}
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'catalog',
+    data () {
+      return {
+        msg: 'template'
+      }
+    },
+    computed: {
+    },
+    props: [],
+    methods: {
+    },
+  }
+</script>
+
+<style lang="sass">
+
+</style>
